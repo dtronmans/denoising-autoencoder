61f42d8a8162aa941b87dd8b9d32ce020329248c
Dani Rogmans <danirogmans@gmail.com>
2025-03-03 15:57:46 +0100

    remove normalization

--- src/config.py
+++ src/config.py
@@ -45,8 +45,7 @@ class Config:
     def parse_architecture_dataset(self):
         self.transforms = transforms.Compose([
             transforms.Resize((384, 672)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            transforms.ToTensor()
         ])
 
         if self.dataset == "UltrasoundDataset":
